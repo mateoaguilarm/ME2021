80fc8e46421697cfb89823834e7be410a2c722b7
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-05-06 19:17:07 -0500

    sass ordered, boottstrap setted

--- sem012/test-react-sem12/src/App.js
+++ sem012/test-react-sem12/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Components
+
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      
+        <Switch>
+          <Navbar />
+          <Route>
+
+          </Route>
+        </Switch>
       </div>
     </BrowserRouter>
     
