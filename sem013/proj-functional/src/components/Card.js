a36222cd44a260e81ca2f1deec1d6674e8de8d0b
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-05-19 22:36:03 -0500

    hooks setted, counter 1/2

--- sem013/proj-functional/src/components/Card.js
+++ sem013/proj-functional/src/components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+import Couter from './Counter';
 import '../Styles/Card.css';
 
 function Card(props) {
--- sem013/proj-functional/src/components/Counter.js
+++ sem013/proj-functional/src/components/Counter.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import '../Styles/Counter.css';
 
 function Counter(props) {
 
+    const [items, setItems] = useState('cart empty')
+
+    useEffect(() => {
+        if (typeof items === 'string') {
+            const cartNumber = document.querySelector('.cart-number');
+            cartNumber.classList.toggle('cart-number--colorNone')
+        } else {
+            const cartNumber = document.querySelector('.cart-number');
+            cartNumber.classList.toggle('cart-number--colorActive')
+        }
+    }, [items])
 
     return (
-        <div style={{ fontSize: '50px'}}>
-            un numero #
+        <div>
+            <div className='cart-number'>
+                {items}
+            </div>
         </div>
     )
 }
