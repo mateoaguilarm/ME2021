dfd5d7f959ad44d98c86c03d92e4011fbe79bf03
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-05-25 21:25:18 -0500

    test.js y .js, first part

---
+++ sem014/TDD/operations.js
@@ -0,0 +1,8 @@
+function sum(num1, num2) {
+    let ops = num1 + num2;
+    return ops
+}
+
+module.exports = {
+    sum
+}
--- sem014/TDD/operations.test.js
+++ sem014/TDD/operations.test.js
@@ -1,14 +1,16 @@
+const { sum } = require('./operations');
+
 // Bloque de Tests los cuales pueden contener varios tests
 describe('Sum test', () => {
     //test('la descripcion del test', ()=>{lo que se testea})
-    test('should print a num', () => {
+    test('Should print a num', () => {
         // en este caso, Se espera un numero
         const expected = 'number';
         // El resultado
-        const result = sum(5, 4);
-
+        const result = sum(5,5);
         // 
-        expect(expected).toBe(typeof result)
+        expect(typeof result).toBe(expected)
+        // hasta aca ya esta la primera parte del test
     })
     
 })
