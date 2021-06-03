34735ac2ff6a807abcf93a17c860b284b8d69fa2
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-06-03 18:20:21 -0500

    sensei code lines added

--- sem015/intro02/index.js
+++ sem015/intro02/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const router = require('./routes');
 
-
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
--- sem015/intro02/routes/index.js
+++ sem015/intro02/routes/index.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
 const router = express.Router();
-
 const { User } = require('../models');
+
 // Routes
 router.get('/', (req, res) => {
     res.status(200).send('hello world');
@@ -17,7 +16,6 @@ router.get('/', (req, res) => {
 
 router.post('/register', async (req, res) => {
     const newUser = new User(req.body)
-
     try {
         const user = await newUser.save()
         res.status(201).send(user)
@@ -26,12 +24,106 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.get('/users/:userid', (req, res) => {
-    res.status(200).send(req.params.userid)
+// router.get('/users/:userid', (req, res) => {
+//     res.status(200).send(req.params.userid)
+// });
+
+// router.get('/users', (req, res) => {
+//     res.status(200).send(req.query)
+// });
+
+router.post('/users', async (req, res) => {
+    const newUser = new User(req.body);
+
+    try {
+        const user = await newUser.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(409).send(error);
+    }
 });
 
-router.get('/users', (req, res) => {
-    res.status(200).send(req.query)
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).send(users)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+})
+
+router.get('/users/:userid', async (req, res) => {
+    const id = req.params.userid;
+
+    try {
+        const user = await User.findById(id);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
+
+router.put('/users/:userid', async (req, res) => {
+    const id = req.params.userid;
+    const newData = req.body;
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, newData, {
+            new: true,
+        });
+
+        res.status(200).send(updatedUser);
+    } catch (error) {
+        res.status(409).send(error);
+    }
+});
+
+router.patch('/users/:userid', async (req, res) => {
+    const id = req.params.userid;
+    const newEmail = req.body.email;
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            { $set: { email: newEmail } },
+            {
+                new: true,
+            },
+        );
+
+        res.status(200).send(updatedUser);
+    } catch (error) {
+        res.status(409).send(error);
+    }
+});
+
+// Esto es un delete fisico, debemos evitar hacer este tipo de deletes
+// router.delete('/users/:userid', async (req, res) => {
+//   try {
+//     await User.findByIdAndDelete(req.params.userid, {new: true});
+//     res.status(200).send({ message: 'User was deleted' });
+//   } catch (error) {
+//     res.status(404).send(error);
+//   }
+// });
+
+// Esto es un delete logico, es el mas recomendado
+router.delete('/users/:userid', async (req, res) => {
+    const id = req.params.userid;
+
+    try {
+        await User.findByIdAndUpdate(
+            id,
+            { $set: { is_active: false } },
+            {
+                new: true,
+            },
+        );
+
+        res.status(200).send({ message: 'User was deleted' });
+    } catch (error) {
+        res.status(404).send(error);
+    }
 });
 
 module.exports = router
