84e15727751b0e3bfdbe4eb22719d38d48efd71f
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-06-02 20:07:03 -0500

    User.js, is on indexConnection

--- sem015/intro02/models/index.js
+++ sem015/intro02/models/index.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb+srv://user-test:1234@backend.65lwy.mongodb.net/bcknd?retryWrites=true&w=majority';
 
+const User = require('./User')
+
 mongoose.connect(mongoURI, (error)=>{
-    error ? console.log(error) : 'Database Connected'
+    error ? console.log(error) : console.log('Database Connected');
 });
 
+module.exports = {
+    User,
+}
